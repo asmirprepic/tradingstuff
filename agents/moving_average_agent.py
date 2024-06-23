1c8720ca7bcd7cc535669849028802729a47b743
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2024-06-23 02:04:09 +0200

    Update moving_average_agent.py
    Fixed some stuff on moving average agent

--- agents/moving_average_agent.py
+++ agents/moving_average_agent.py
@@ -13,10 +13,11 @@ class MovingAverageAgent(TradingAgent):
         back_length (int, optional): The number of periods to calculate the moving average. Defaults to 20.
     """
 
-    def __init__(self, data, back_length=20):
+    def __init__(self, data, short_window=50,long_window=200):
         super().__init__(data)
         self.algorithm_name = "MovingAverage"
-        self.back_length = back_length
+        self.short_window = short_window
+        self.long_window = long_window
         self.price_type = 'Close'
 
         self.stocks_in_data = self.data.columns.get_level_values(0).unique()
@@ -39,13 +40,13 @@ class MovingAverageAgent(TradingAgent):
         signals = pd.DataFrame(index=self.data.index)
         close_price = self.data[(stock,'Close')]
         signals['price'] = close_price
-        signals['SMA'] = close_price.rolling(window=self.back_length).mean()
+        signals['SMA_short'] = close_price.rolling(window=self.short_window).mean()
+        signals['SMA_long'] = close_price.rolling(window=self.long_window).mean()
 
         
         # Generate signals for only long positions
         signals['Position'] = 0
-        signals.loc[(signals['price'] > signals['SMA']), 'Position'] = 1  # Long position
-        signals.loc[(signals['price'] < signals['SMA']), 'Position'] = 0  # Exit position
+        signals['Position'] = np.where(signals['SMA_short'] > signals['SMA_long'], 1, 0)
         
         signals['Signal'] = signals['Position'].diff()
         signals['return'] = np.log(self.data[(stock,'Close')]/self.data[(stock,'Close')].shift(1))
@@ -54,14 +55,14 @@ class MovingAverageAgent(TradingAgent):
         signals['Position'] = signals['Position'].ffill()
         
         
-        signals['buy'] = (signals['Signal']==1) & (signals['Signal'].shift(1)==0)
-        signals['sell'] = (signals['Signal']==0) & (signals['Signal'].shift(1)==1)
+        signals['buy'] = (signals['Signal']==1)
+        signals['sell'] = (signals['Signal']==-1)
 
         signals['hold_start'] = signals['buy'].cumsum()
         signals['hold_end'] = signals['sell'].cumsum()
 
         signals['position'] = (signals['hold_start']>signals['hold_end']).astype(int)
-        
+         
         
         # Determine when the price crosses the SMA
         #condition = (self.data[stock].iloc[self.back_length:] > signals['SMA'].iloc[self.back_length:]).values
@@ -71,11 +72,12 @@ class MovingAverageAgent(TradingAgent):
 
     def plot(self,stock):
       fig,ax = super().plot(stock)
-      sma = self.data[(stock, 'Close')].rolling(window=self.back_length).mean()
-      ax.plot(self.data.index, sma, label=f'{self.back_length}-Day SMA', color='orange')
+      sma_short = self.data[(stock, 'Close')].rolling(window=self.short_window).mean()
+      sma_long = self.data[(stock, 'Close')].rolling(window=self.long_window).mean()
+      ax.plot(self.data.index, sma_short, label=f'{self.short_window}-Day SMA', color='blue', linewidth=1.5, linestyle='--')
+      ax.plot(self.data.index, sma_long, label=f'{self.long_window}-Day SMA', color='orange', linewidth=1.5, linestyle='--')
       #plt.plot(self.data.index,self.signal_data[(stock,'Close']['position'])
+
+
       plt.legend()
       return fig,ax
-
-
-
